aa9fc7d6d2bf0b45c8e49f5fe167f8c9db727a77
wandergithub <wandergonzalez0@hotmail.com>
2024-05-17 17:41:49 -0400

    Add expenses: - Controller - Model - routes - migrations - schema - test files

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_211946) do
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
