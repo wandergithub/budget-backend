a4d20f0a757e3970ece99bac0fe26f3b75be1dec
wandergithub <wandergonzalez0@hotmail.com>
2024-05-24 15:26:45 -0400

    Add fake gem and generated a year worth of ramdom data added chartdata endpoint method and action to retrieve a year worth of data separated by monthly income and expenses

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -31,6 +31,24 @@ class TransactionsController < ApplicationController
             render json: @transaction.errors, status: :unprocessable_entity
         end
     end
+# Returns last transactions ordered by months in a year period, separate income and expense... expense on positive values
+    def chart_data
+        end_date = Transaction.all.last.updated_at
+        start_date = Transaction.all.first.updated_at - 1.year
+        transactions = Transaction.where(updated_at: start_date.beginning_of_day..end_date.end_of_day).order(updated_at: :asc).group_by { |t| t.updated_at.beginning_of_month }
+        monthly_transactions = transactions.transform_values do |entries| {
+            income: entries.select {|t| t.value > 0},
+            expense: entries.select {|t| t.value < 0}
+        }
+        end
+
+        @response = monthly_transactions
+        if @response
+            render json: @response, status: :ok
+        else
+            render json: @response, status: :unprocessable_entity
+        end
+    end
 
     private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+
+# Clear existing data
+Transaction.delete_all
+
+# Define the start date
+start_date = Date.today - 1.year
+
+# Generate data for each day of the past year
+(start_date..Date.today).each do |date|
+  # Create a random number of posts per day
+  rand(1..5).times do
+    Transaction.create!(
+      name: Faker::Lorem.sentence(word_count: 1),
+      value: Faker::Number.between(from: -10000, to: 10000),
+      created_at: date,
+      updated_at: date
+    )
+  end
+end
+
+puts "Seeded a year's worth of data."
